ac9482346ce64dfe180320659a37346c724e7211
dkorbey <demirkanbaglamac@gmail.com>
2020-12-09 18:20:33 +0100

    dsfsdf

---
+++ js/slider.js
@@ -0,0 +1,11 @@
+
+
+var slider_texts = {
+    "London DevOps Meetup":   ["image/sliders/slide1.jpg","Lorem ipsum dolor sit amet consectetur adip"] , 
+    "two":   ["image/sliders/slide1.jpg","2"],
+    "three": ["image/sliders/slide1.jpg","3"],
+    "four":  ["image/sliders/slide1.jpg","4"],
+    "five":  ["image/sliders/slide1.jpg","5"]
+}
+var active_slide = document.getElementById("")
+
