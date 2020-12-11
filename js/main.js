153ae99147c63c70d16d14edab2c523992e13959
Yusuf Anı <yusufani8@gmail.com>
2020-12-11 01:50:50 +0100

    s

--- js/main.js
+++ js/main.js
@@ -3,7 +3,8 @@ window.addEventListener('DOMContentLoaded', (event) => {
     search_button = document.getElementById("search_button")
     $("#search_button").on('keyup', function (e) {
         if (e.key === 'Enter' || e.keyCode === 13) {
-            document.location.href = "/htmls/tracking.html?"+search_button.value;
+            console.log(document.location)
+            //document.location.href = "/htmls/tracking.html?"+search_button.value;
         }
     });
 });
