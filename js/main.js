5b14675b47bbb3674ecd8a399e975d68191201f4
Yusuf ANI <yusufani8@gmail.com>
2020-12-14 18:55:45 +0100

    Removed commments

--- js/main.js
+++ js/main.js
@@ -21,7 +21,6 @@ window.addEventListener('DOMContentLoaded', (event) => {
 
 
 function change_visibility_sbutton(el){
-    console.log(el)
     if (el === 0) {
          search_button.style.visibility = "hidden";  
     }else{
